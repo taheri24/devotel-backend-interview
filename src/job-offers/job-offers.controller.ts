86804cb26f4a25484569023f6de88f2493ea6e07
Majid Taheri(I love optimization) <majid.taheri@qlub.io>
2025-02-16 15:43:13 +0330

    add crud to joboffer

--- src/job-offers/job-offers.controller.ts
+++ src/job-offers/job-offers.controller.ts
@@ -1,14 +1,54 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, Query, Res } from '@nestjs/common';
 import { JobOffersService } from './job-offers.service';
 import { JobOffer } from '~/model/job-offer.entity';
-
+import { Response } from 'express';
+const validInteger = (s: string) => {
+    if (!s) return 0;
+    const n = parseInt(s, 10);
+    if (isNaN(n)) throw new Error(`invalid number`);
+    return n
+}
 @Controller('job-offers')
 export class JobOffersController {
-    constructor(private readonly jobOffersService: JobOffersService) {}
+    constructor(private readonly jobOffersService: JobOffersService) { }
+
     @Get()
-    getHello(): Array<JobOffer> {
-        return [];
-      }
+    async getJobs(@Query('skip', { transform: validInteger }) skip: number,
+        @Query('pageNo', { transform: validInteger }) take: number,
+        @Res() res: Response): Promise<void> {
+        take = take || 10;
+        const [list, count] = await this.jobOffersService.getJobsWithPaginate(skip, take);
+        console.log('list:', list, 'count:', count);
+        res.setHeader('X-Total-Count', count);
+        res.json(list);
+    }
+
+    @Post()
+    createJob(@Body() jobOffer: JobOffer): Promise<JobOffer> {
+               
+        return this.jobOffersService.createJob(jobOffer);
+    }
+
+    @Get(':id')
+    async getJob(@Param('id') id: string): Promise<JobOffer> {
+        const jobOffer = await this.jobOffersService.getJob(id);
+        if (!jobOffer) {
+            throw new NotFoundException(`Job offer not found with ID ${id}`);
+        }
+
+        return jobOffer;
+    }
+
+    @Put(':id')
+    async updateJob(@Param('id') id: string, @Body() jobOffer: JobOffer): Promise<JobOffer> {
+        return this.jobOffersService.updateJob(id, jobOffer);
+    }
+
+    @Delete(':id')
+    deleteJob(@Param('id') id: string): void {
+        this.jobOffersService.deleteJob(id);
+    }
+
     @Post('trigger-cron')
     async triggerCron() {
         await this.jobOffersService.triggerCron();
--- src/job-offers/job-offers.service.ts
+++ src/job-offers/job-offers.service.ts
@@ -10,78 +10,114 @@ import { InjectRepository } from '@nestjs/typeorm';
 export type UpsertItem = { ref: string, manipulateType: 'insert' | 'update', data: JobOffer, id: number };
 @Injectable()
 export class JobOffersService {
-    constructor(
-        private a: SourceA,
-        private b: SourceB,
-        @InjectRepository(JobOffer) private jobOfferRepository: Repository<JobOffer> // Use generic TypeORM repository
-    ) { }
+  constructor(
+    private a: SourceA,
+    private b: SourceB,
+    @InjectRepository(JobOffer) private jobOfferRepository: Repository<JobOffer> // Use generic TypeORM repository
+  ) { }
 
-    async collect(collectors: JobCollector[]): Promise<Array<JobOffer>> {
-        return (await Promise.all(collectors.map(c => c.collect()))).flat();
+  async createJob(jobOffer: JobOffer): Promise<JobOffer> {
+    if (jobOffer.id) {
+      throw new Error('Id should not be provided when creating a new job offer');
     }
+    return this.jobOfferRepository.save(jobOffer);
+  }
 
-    async triggerCron() {
-        await this.handleCron();
+  async getJob(id: string): Promise<JobOffer | null> {
+    return this.jobOfferRepository.findOne({ where: { id: parseInt(id) } });
+  }
+
+  async updateJob(id: string, jobOffer: JobOffer): Promise<JobOffer> {
+    const currJobOfferOnDb = await this.jobOfferRepository.findOne({ where: { id: parseInt(id) } });
+    if (JSON.stringify(currJobOfferOnDb) == JSON.stringify(jobOffer)) {
+      throw new Error('Job offer is the same with the one on the database');
     }
-
-    @Cron('0 * * * *') // Every hour
-    async handleCron() {
-        const collectedJobs = await this.collect([this.a, this.b]);
-        console.log('Collected jobs:', collectedJobs);
-         for await( const updateItem of  this.upsertJobs(collectedJobs)){
-            console.log('Upserted job:', updateItem);
-        }
-        return collectedJobs
+    const {affected}= await this.jobOfferRepository.update(parseInt(id), jobOffer);
+    if (!affected){
+      throw new Error('update entity failed,Job offer not found ');
     }
-
-    async *upsertJobs(jobs: JobOffer[]): AsyncIterable<UpsertItem> {
-        for (const job of jobs) {
-             const matchedJob = await this.jobOfferRepository.findOne({
-                where:
-                {
-                  path:job.GetPath()
-                }
-            });
-            if (!job.industry){ 
-                const revisedIndustry=await this.refineIndustry(job)
-                if (revisedIndustry) job.industry=revisedIndustry;
-            }  
-            if (matchedJob?.id) {
-                // Update the existing job
-                await this.jobOfferRepository.update(matchedJob.id, job);
-                yield { ref: job.ref, data: job, manipulateType: 'update', id: matchedJob.id } as UpsertItem;
-            } else {
-                // Insert the new job
-                await this.jobOfferRepository.insert(job);
-                yield { ref: job.ref, data: job, manipulateType: 'insert', id: job.id } as UpsertItem;
-
-            }
-        }
-    
-    
+    const reloadedJobOfferOnDb = await this.jobOfferRepository.findOne({ where: { id: parseInt(id) } });
+    if (!reloadedJobOfferOnDb) {
+      throw new Error('reload entity failed,Job offer not found ');
     }
-    async refineIndustry(jobOffer: JobOffer): Promise<string | null> {
-        if (jobOffer.industry) {
-          return jobOffer.industry;
+    return reloadedJobOfferOnDb;
+  }
+
+  async deleteJob(id: string): Promise<void> {
+    await this.jobOfferRepository.delete(id);
+  }
+
+  async collect(collectors: JobCollector[]): Promise<Array<JobOffer>> {
+    return (await Promise.all(collectors.map(c => c.collect()))).flat();
+  }
+
+  async triggerCron() {
+    await this.handleCron();
+  }
+
+  @Cron('0 * * * *') // Every hour
+  async handleCron() {
+    const collectedJobs = await this.collect([this.a, this.b]);
+    console.log('Collected jobs:', collectedJobs);
+    for await (const updateItem of this.upsertJobs(collectedJobs)) {
+      console.log('Upserted job:', updateItem);
+    }
+    return collectedJobs
+  }
+
+  async getJobsWithPaginate(skip, take: number): Promise<[Array<JobOffer>,number]> {
+
+    return this.jobOfferRepository.findAndCount({skip,take,order:{id:'ASC'}});
+  }
+
+  async *upsertJobs(jobs: JobOffer[]): AsyncIterable<UpsertItem> {
+    for (const job of jobs) {
+      const matchedJob = await this.jobOfferRepository.findOne({
+        where:
+        {
+          path: job.GetPath()
         }
-        const filters: Array<FindOptionsWhere<JobOffer>> = [
-          { company: jobOffer.company, title: jobOffer.title },
-          { title: jobOffer.title },
-          { company: jobOffer.company }
-        ];
-    
-        for (const where of filters) {
-          const result = await this.jobOfferRepository.findOne({
-            where: {
-              ...where,
-              industry: Not(IsNull()) // Use Not(IsNull()) to replace invalid null value
-            },
-            select: ['industry']
-          });
-          if (result?.industry) {
-            return result.industry;
-          }
-        }
-        return null; // Default value if no industry is found
+      });
+      if (!job.industry) {
+        const revisedIndustry = await this.refineIndustry(job)
+        if (revisedIndustry) job.industry = revisedIndustry;
       }
+      if (matchedJob?.id) {
+        // Update the existing job
+        await this.jobOfferRepository.update(matchedJob.id, job);
+        yield { ref: job.ref, data: job, manipulateType: 'update', id: matchedJob.id } as UpsertItem;
+      } else {
+        // Insert the new job
+        await this.jobOfferRepository.insert(job);
+        yield { ref: job.ref, data: job, manipulateType: 'insert', id: job.id } as UpsertItem;
+
+      }
+    }
+
+
+  }
+  async refineIndustry(jobOffer: JobOffer): Promise<string | null> {
+    if (jobOffer.industry) {
+      return jobOffer.industry;
+    }
+    const filters: Array<FindOptionsWhere<JobOffer>> = [
+      { company: jobOffer.company, title: jobOffer.title },
+      { title: jobOffer.title },
+      { company: jobOffer.company }
+    ];
+
+    for (const where of filters) {
+      const result = await this.jobOfferRepository.findOne({
+        where: {
+          ...where,
+          industry: Not(IsNull()) // Use Not(IsNull()) to replace invalid null value
+        },
+        select: ['industry']
+      });
+      if (result?.industry) {
+        return result.industry;
+      }
+    }
+    return null; // Default value if no industry is found
+  }
 }
